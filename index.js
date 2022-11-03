e3b8604de7da17c1df0af19c124b9644c50a4c5c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-11-03 23:36:45 +0530

    Variables done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('Hello');
+
+let name = "Dul";
+console.log(name);
+
+//can't start with a number
+//souldn't be include spaces or - 
+//should be meaningfull
+//can't be a reserved keywords
+//case sensitive
+
+let firstName = "Dula";
+let secName = "Dil";
