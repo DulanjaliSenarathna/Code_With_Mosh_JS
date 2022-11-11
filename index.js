cb6bf309d87a58c4d167593da2a9bc79a9a730ef
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-11-11 20:57:20 +0530

    Exercise 5- Even and Odd Numbers

--- index.js
+++ index.js
@@ -397,3 +397,17 @@ function checkSpeed(speed){
 
 checkSpeed(75);
 
+//Exercise 5- Even and Odd Numbers
+
+function showNumbers(limit){
+    for(let i=0; i<=limit; i++){
+        if(i%2==0){
+            console.log(i+' EVEN');
+        } else{
+            console.log(i+' ODD');
+        }
+    }
+}
+
+showNumbers(10);
+
